396b29177350503c3191d85b1582b25fe5a9a964
Eugene <eugenechigozieanyata@gmail.com>
2025-02-06 09:55:52 +0100

    modified my vite config file to contain project url, added a footer tag with icon information, added a favicon to my index.html, positioned the footer tag

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JournalApp/',
+  base: 'https://zanyachi.github.io/JournalApp/',
   plugins: [react()],
 })
